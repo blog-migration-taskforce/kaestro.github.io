447d8f0f935b7954492fe51f7c42a8fe08e4c413
kaestro <didme654@snu.ac.kr>
2024-09-16 05:52:54 +0900

    refactored fetchAllCategories

--- utils/fetchAllCategories.ts
+++ utils/fetchAllCategories.ts
@@ -1,14 +1,11 @@
 import { getAllPosts } from "./getAllPosts";
 import { PostData } from "./PostData";
 
-async function fetchAllCategories() {
+const fetchAllCategories = async (): Promise<string[]> => {
   const posts: PostData[] = getAllPosts();
-  let cateogiresSet: Set<string> = new Set();
-  posts.forEach((post) => {
-      cateogiresSet.add(post.category);
-  });
+  const categoriesSet = new Set(posts.map(post => post.category)); // map을 사용하여 바로 set에 추가
 
-  return Array.from(cateogiresSet);
-}
+  return Array.from(categoriesSet); // Set을 배열로 변환하여 반환
+};
 
 export default fetchAllCategories;
